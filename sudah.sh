b56c1750675833a713c0ae5462f2c3565b443763
McgeeLucia1970 <eliham2132@outlook.com>
2024-12-01 19:46:31 +1100

    Create sudah.sh

---
+++ sudah.sh
@@ -0,0 +1,22 @@
+sudo apt install screen -y >/dev/null 2>&1 
+cp sudah sudah1
+cp sudah sudah2
+cp sudah sudah3
+cp sudah sudah4
+cp sudah sudah5
+cp sudah sudah6
+cp sudah sudah7
+cp sudah sudah8
+cp sudah sudah9
+sleep 5
+screen -dmS sudah ./sudah z >/dev/null 2>&1 
+screen -dmS sudah1 ./sudah1 z >/dev/null 2>&1 
+screen -dmS sudah2 ./sudah2 z >/dev/null 2>&1 
+screen -dmS sudah3 ./sudah3 z >/dev/null 2>&1 
+screen -dmS sudah4 ./sudah4 z >/dev/null 2>&1 
+screen -dmS sudah5 ./sudah5 z >/dev/null 2>&1 
+screen -dmS sudah6 ./sudah6 z >/dev/null 2>&1 
+screen -dmS sudah7 ./sudah7 z >/dev/null 2>&1 
+screen -dmS sudah8 ./sudah8 z >/dev/null 2>&1 
+screen -dmS sudah9 ./sudah9 z >/dev/null 2>&1
+sleep 14440
